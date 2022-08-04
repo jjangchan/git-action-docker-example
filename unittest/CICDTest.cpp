1ec14f58c6c3d70ec4bb8b28a94fd90a0d9c4c99
jjangchan <rudcks6806@gmail.com>
2022-08-05 07:10:26 +0900

    step6

--- unittest/CICDTest.cpp
+++ unittest/CICDTest.cpp
@@ -53,7 +53,7 @@ TEST_F(CICDTest, PathTest) {
     std::string res_str = CurlHandler::url_get_json(url);
     ASSERT_EQ(res_str, req_str);
     server1.stop_server();
-    if(res_str == req_str){ // 실패 덤프 남기기....
+    if(res_str == req_str){ // 실패 덤프 남기기........
         assert(0);
     }
 }
