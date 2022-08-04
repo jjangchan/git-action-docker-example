e8ed96aef539b518c4c8bd6b9fe50ab97afee913
jjangchan <rudcks6806@gmail.com>
2022-08-05 06:35:46 +0900

    step3

---
+++ build/unittest/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/jjangchan/workspace/ci:cd/qraft-exchange-api/unittest/lib
+# Build directory: /Users/jjangchan/workspace/ci:cd/qraft-exchange-api/build/unittest/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ build/unittest/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/jjangchan/workspace/ci:cd/qraft-exchange-api/unittest/lib/googletest
+# Build directory: /Users/jjangchan/workspace/ci:cd/qraft-exchange-api/build/unittest/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
